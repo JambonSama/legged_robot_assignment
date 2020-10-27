c50e0568c0b00c9f0aef9fed0555b8f241564738
Claire Meyer <clairemeyerbourhis@gmail.com>
2020-10-27 12:04:46 +0100

    added : Assignment 4 files (pdf for assignment and code files).

---
+++ assignment_04/code/analysis/analyse.m
@@ -0,0 +1,21 @@
+function results = analyse(sln, parameters, to_plot)
+
+% calculate gait quality metrics (distance, step frequency, step length,
+% velocity, etc.)
+
+% calculate actuation (you can use the control function)
+
+if to_plot
+    % plot the angles
+
+    % plot the hip position
+    
+    % plot instantaneous and average velocity
+    
+    % plot projections of the limit cycle
+   
+    % plot actuation
+    
+end
+
+end
