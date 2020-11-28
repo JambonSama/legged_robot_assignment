6a4bb671159e8a6c804e9e9ef6859d2bea955f42
Claire Meyer <clairemeyerbourhis@gmail.com>
2020-11-28 18:21:01 +0100

    modified : Two figure outputs for assignments 1 and 2, for the report.

--- assignment_02/three_link_biped_start/visualize/visualize.m
+++ assignment_02/three_link_biped_start/visualize/visualize.m
@@ -1,15 +1,13 @@
 %%
-% This function takes the configuration of the 3-link model and plots the 
-% 3-link model. 
+% This function takes the configuration of the 3-link model and plots the
+% 3-link model.
 % q = [q1, q2 ,q3] the generalized coordinates. Try different angles to see
 % if your formulas for x1, z1, etc. makes sense. Example: q = [-pi/6, pi/6,
 % pi/8]
 %%
 function visualize(q)
-%     [~, ~, ~, l1, l2, l3, ~] = set_parameters;
-    l1 = 0.75;
-    l2 = 0.75;
-    l3 = 0.5;
+    addpath '/mnt/data/epfl/legged_robots/assignments/assignment_02/three_link_biped_start/visualize'
+    [~, ~, ~, l1, l2, l3, ~] = set_parameters;
 
     q1 = q(1);
     q2 = q(2);
@@ -29,7 +27,7 @@ function visualize(q)
     x_t = l1*sin(q1) +l3*sin(q3);
     z_t = l1*cos(q1) +l3*cos(q3);
 
-    %% 
+    %%
     % Here plot a schematic of the configuration of three link biped at the
     % generalized coordinate q = [q1, q2, q3]:
     close all
@@ -39,17 +37,21 @@ function visualize(q)
     hold on
 
     % links
-    plot([0, x_h], [0, z_h], 'linewidth', line_width); 
-    plot([x_h, x_swf], [z_h, z_swf], 'linewidth', line_width); 
-    plot([x_h, x_t], [z_h, z_t], 'linewidth', line_width); 
+    plot([0, x_h], [0, z_h], 'linewidth', line_width);
+    plot([x_h, x_swf], [z_h, z_swf], 'linewidth', line_width);
+    plot([x_h, x_t], [z_h, z_t], 'linewidth', line_width);
 
     % point masses
-    plot(x1, z1, '.', 'markersize', marker_size); 
-    plot(x2, z2, '.', 'markersize', marker_size); 
-    plot(x3, z3, '.', 'markersize', marker_size); 
+    plot(x1, z1, '.', 'markersize', marker_size, 'color', '#0072BD');
+    plot(x2, z2, '.', 'markersize', marker_size, 'color', '#D95319');
+    plot(x3, z3, '.', 'markersize', marker_size, 'color', '#EDB120');
 
     % appearance
-    axis 'square'
-    xlim([-1, 1]);
-    ylim([0, 1.2]);
+    axis 'equal'
+    xlim([-0.8, 0.2]);
+    ylim([0, 1]);
+    xlabel("x","interpreter","latex")
+    ylabel("y","interpreter","latex")
+    xticks([])
+    yticks([])
 end
