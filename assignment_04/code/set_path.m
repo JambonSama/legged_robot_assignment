8c814f1e6576295ec2b5921683c1014b3f255b30
Mathieu Berthet <berthet.mathieu@gmail.com>
2020-12-02 14:08:59 +0100

    Finished the loop I think, must check the results with output of data
    Added the impact block to the loop, the control function (is to be removed later on when the Agent is put in place), forced the input and outputs to be the correct dimension, added a block for initialization and put the initial conditions in set_path

--- assignment_04/code/set_path.m
+++ assignment_04/code/set_path.m
@@ -1,2 +1,6 @@
 addpath('./kinematics', './control', './dynamics', './set_parameters/', ...
     './solve_eqns/', './visualize', './analysis', './optimization')
+
+q0 = [pi/9; -pi/9; 0];
+dq0 = [0; 0; 8];
+x0 = [q0;dq0];
