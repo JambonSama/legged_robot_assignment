0c6cc0ff5181b6fc7482c529de35cb91f46547a3
Nicolas <nicolas.furrer@epfl.ch>
2020-12-08 10:42:18 +0100

    Added : code for kinematic top point Modified : code for control to include kinematic top

--- assignment_05/code/control/control.m
+++ assignment_05/code/control/control.m
@@ -12,17 +12,20 @@ dxd = parameters(6);
 zd = parameters(7);
 dzd = parameters(8);
 B = eval_B();
-[~, ~, ~, l1, ~, ~, ~] = set_parameters();
+[~, ~, ~, l1, ~, l3, ~] = set_parameters();
 [x_h, z_h, dx_h, dz_h] = kin_hip(q, dq);
+[x_t, z_t, dx_t, dz_t] = kin_top(q, dq)
 
 % Task space projection
 J_h = [l1*cos(q(1)), 0, 0; -l1*sin(q(1)), 0, 0]; % at the hip
-%J_h = [l1*cos(q(1)), 0, l3*cos(q(3)); -l1*sin(q(1)), 0, -l3*sin(q(3))];% point on top
-f_h = [kpx*(xd-x_h)+kdx*(dxd-dx_h); kpz*(zd-z_h)+kdz*(dzd-dz_h)];
+J_h2 = [l1*cos(q(1)), 0, l3*cos(q(3)); -l1*sin(q(1)), 0, -l3*sin(q(3))];% point on top
+f_h2 = [kpx*(x_h-x_t)+kdx*(dxd-dx_h); kpz*(zd-z_h)+kdz*(dzd-dz_h)];
+f_h = [kpx*(xd-x_h)+kdx*(dx_h-dx_t); 0];
+
 u = pinv(B)*J_h'*f_h;
 
 % saturate the output torque
 %u = [u1; u2];
-u = max(min(u, 30), -30);
+u = max(min(u, 30), -30)
 
 end
--- assignment_05/code/control/control_hyper_parameters.m
+++ assignment_05/code/control/control_hyper_parameters.m
@@ -2,12 +2,12 @@
 % may not want to use the step_number as the input of the function. 
 function parameters = control_hyper_parameters(step_number)
 kpx = 50; % a opti
-kpz = 50; % a opti
+kpz = 0; % a opti
 kdx = 50; % a opti
-kdz = 50; % a opti
-xd = 0.5*cos(pi/9); % ? voir ?
-dxd = 5; % a opti
-zd = 0; % normalement 0
+kdz = 0; % a opti
+xd = 0.345; % ? voir ?
+dxd = 0.5; % a opti
+zd = 0.5*cos(pi/9)+0.35; % normalement 0
 dzd = 0; % normalement 0
 
 
