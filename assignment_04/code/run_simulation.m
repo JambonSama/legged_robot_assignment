6ab7ec26cb02991c38250621043aa0366f9ad9eb
Claire Meyer <clairemeyerbourhis@gmail.com>
2020-12-08 16:48:19 +0100

    reverted : assignment_04 folder back to just assignment 4.

--- assignment_04/code/run_simulation.m
+++ assignment_04/code/run_simulation.m
@@ -9,5 +9,5 @@ num_steps = 10;
 
 default_parameters = control_hyper_parameters(num_steps);
 sln = solve_eqns(q0, dq0, num_steps, default_parameters);
-animate(sln);
+%%animate(sln);
 analyse(sln, default_parameters, true);
