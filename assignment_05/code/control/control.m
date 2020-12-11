0739cb2748e36c7cb9e2e5d86999ac91cad21785
Nicolas <nicolas.furrer@epfl.ch>
2020-12-12 00:49:24 +0100

    Modified : update swing controleur

--- assignment_05/code/control/control.m
+++ assignment_05/code/control/control.m
@@ -2,55 +2,77 @@ function u = control(t, q, dq, q0, dq0, step_number, parameters)
 % You may call control_hyper_parameters and desired_outputs in this function
 % you don't necessarily need to use all the inputs to this control function
 
-% extract controler parameters
-kdx_h = parameters(1); 
-
-kpx_t = parameters(2); 
-kdx_t = parameters(3);
-
-kpx_s = parameters(4);
-kdx_s = parameters(5);
-kpz_s = parameters(6);
-kdz_s = parameters(7);
-
-% step trajectory
-h = parameters(8);
-step_length = parameters(9);
-speed_hip = parameters(10);
-speed_swf = parameters(11);
-
-% compute value
+% Computation value
 B = eval_B();
 [~, ~, ~, l1, l2, l3, ~] = set_parameters();
-[x_swf, z_swf, dx_swf, dz_swf] = kin_swf(q, dq);
-[x0_swf, ~, ~, ~] = kin_swf(q0, dq0);
 [x_h, z_h, dx_h, dz_h] = kin_hip(q, dq);
 [x_t, z_t, dx_t, dz_t] = kin_top(q, dq);
-z_swf_target = h *sin(pi/step_length*(x_swf-x0_swf));
+[x_swf, z_swf, dx_swf, dz_swf] = kin_swf(q, dq);
 
+%% Hip task
 
+% Hip parameters
+speed_hip = parameters(1);
+kdx_h = parameters(2); 
 
 % Task space projection Hip point
 J_h = [l1*cos(q(1)), 0, 0; -l1*sin(q(1)), 0, 0];
 f_h = [kdx_h*(speed_hip-dx_h); 0];
 u_h = pinv(B)*J_h'*f_h;
 
+%% Top task
+
+% Top parameters
+kpx_t = parameters(3); 
+kdx_t = parameters(4);
+kpz_t = parameters(5);
+kdz_t = parameters(6);
+leaning_angle = parameters(7);
+
+% Computation ref value Top
+x_top_t = x_h + sin(leaning_angle);
+z_top_t = z_h + cos(leaning_angle);
+
 % Task space projection top point
 J_t = [l1*cos(q(1)), 0, l3*cos(q(3)); -l1*sin(q(1)), 0, -l3*sin(q(3))];
-f_t = [kpx_t*(x_h-x_t)+kdx_t*(dx_h-dx_t); 0];
+f_t = [kpx_t*(x_top_t-x_t)+kdx_t*(-dx_t); kpz_t*(z_top_t-z_t)+kdz_t*(-dz_t)];
 u_t = pinv(B)*J_t'*f_t;
 
+
+%% Swing foot task
+
+% Swing foot parameters
+kpx_s = parameters(8);
+kdx_s = parameters(9);
+kpz_s = parameters(10);
+kdz_s = parameters(11);
+h = parameters(12);
+step_length = parameters(13);
+speed_swf = parameters(14);
+
+% Computation ref value Swing foot
+x_swf_tc = -step_length/2:0.1:step_length/2;
+z_swf_tc = h*cos(2*pi/step_length*x_swf_tc)+h/2;
+swf_norm =zeros(numel(x_swf_tc),1);
+
+for i=1:numel(x_swf_tc)
+    swf_norm(i) = norm([x_swf z_swf]-[x_swf_tc(i) z_swf_tc(i)]);
+end
+[~, idx] = min(swf_norm);
+x_swf_t = x_swf_tc(idx);
+z_swf_t = z_swf_tc(idx);
+
 % Task space projection swing point
-J_swf = [l1*cos(q(1)), -l2*cos(q(2)), 0; l2*sin(q(2)), -l1*sin(q(1)), 0];
-f_swf = [kdx_s*(speed_swf); kpz_s*(z_swf_target-z_swf)];
+J_swf = [l1*cos(q(1)), -l2*cos(q(2)), 0; -l1*sin(q(1)), l2*sin(q(2)), 0];
+f_swf = [kpx_s*(x_swf_t-x_swf)+kdx_s*(speed_swf-dx_swf); kpz_s*(z_swf_t-z_swf)];
 u_swf = pinv(B)*J_swf'*f_swf;
 
 
-% Compute command
+%% Compute command
 u = u_t + u_h + u_swf;
 
 % saturate the output torque
 %u = [u1; u2];
-u = max(min(u, 30), -30);
+%u = max(min(u, 30), -30)
 
 end
--- assignment_05/code/control/control_hyper_parameters.m
+++ assignment_05/code/control/control_hyper_parameters.m
@@ -1,24 +1,28 @@
 % You can set any hyper parameters of the control function here; you may or
 % may not want to use the step_number as the input of the function. 
 function parameters = control_hyper_parameters(step_number)
+
 % Parameters for Hip task : speed hip
-kdx_h = 0; % a opti
+speed_hip = 0.2;
+kdx_h = 50;
+
 % Parameters for Top task
-kpx_t = 500; % a opti
-kdx_t = 0; % a opti
+kpx_t = 500; 
+kdx_t = 200;
+kpz_t = 500;
+kdz_t = 200;
+leaning_angle = deg2rad(10);
+
 % Parameters for swing task
-kpx_s = 0.345; % ? voir ?
-kdx_s = 0.5; % a opti
-kpz_s = 0;
-kdz_s = 0;
-
-% step trajectory
-h = 0; % normalement 0
-step_length = 0; % normalement 0
-speed_hip = 0;
-speed_swf = 0;
+kpx_s = 50;
+kdx_s = 20;
+kpz_s = 50;
+kdz_s = 20;
+h = 0.05;
+step_length = sin(pi/9)*0.5*2;
+speed_swf = 0.4;
 
 
-parameters = [kdx_h, kpx_t, kdx_t, kpx_s, kdx_s, kpz_s, kdz_s, h, step_length, speed_hip, speed_swf]';
+parameters = [speed_hip, kdx_h, kpx_t, kdx_t, kpz_t, kdz_t, leaning_angle, kpx_s, kdx_s, kpz_s, kdz_s, h, step_length, speed_swf]';
 
 end
